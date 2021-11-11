546c3840917fce1315f357fa64609fbfd243653e
Love001 <oltyhope2@gmail.com>
2021-11-11 10:14:29 +0100

    first update

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combine'));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/api/v1', (req, res)=>{
+    res.json({message: "hello there"});
+});
+app.post('/api/v1', (req, res)=>{
+    res.json({message: "hello there "+req.body.email});
+});
+
+const port = process.env.PORT || 8081;
+app.listen(port, ()=>console.log(`Port listening on ${port}`));
